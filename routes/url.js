f62871c1e80a4b6ee50d04f7853374a401e70f7d
Yash-Rawat-IMA <yash97181004@gmail.com>
2025-09-21 12:09:11 +0530

    Removed this code and going to update a changed one

--- routes/url.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const { generateNewURL } = require('../controller/url')
-
-router.post('/', generateNewURL);
-module.exports = router;
